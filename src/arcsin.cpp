164d8b0ca0176a2a57165cdcab2ab383e16ff5f3
chimi <deadxraver@yandex.ru>
2026-03-07 00:22:34 +0300

    improve accuracy, use tests for doubles

--- src/arcsin.cpp
+++ src/arcsin.cpp
@@ -16,13 +16,14 @@ double arcsin_power(double x, unsigned int n) {
   if (x > 1 || x < -1) {
     throw std::string("|x| > 1");
   }
-  if (n > 85) {
-    throw std::string("n > 85, double won't handle this...");
+  double term = x;
+  double sum = term;
+  double x2 = x * x;
+
+  for (unsigned int i = 1; i < n; ++i) {
+    term *= (2.0 * i - 1.0) * (2.0 * i - 1.0) * (2.0 * i) * x2 /
+            (4.0 * i * i * (2.0 * i + 1.0));
+    sum += term;
   }
-  double res = 0;
-  for (unsigned int i = 0; i < n; ++i) {
-    res += factorial(2 * i) / (std::pow(2, 2 * i) * std::pow(factorial(i), 2))
-      * std::pow(x, 2 * i + 1) / (2 * i + 1);
-  }
-  return res;
+  return sum;
 }
--- test/arcsin_test.cpp
+++ test/arcsin_test.cpp
@@ -5,11 +5,11 @@
 
 void ArcsinTestCase::overflow_test() {
   CPPUNIT_ASSERT_THROW(arcsin_power(1.1, 10), std::string);
-  CPPUNIT_ASSERT_THROW(arcsin_power(0.5, 86), std::string);
   CPPUNIT_ASSERT_NO_THROW(arcsin_power(0.5, 15));
 }
 
 void ArcsinTestCase::result_test() {
-  CPPUNIT_ASSERT_EQUAL(0.0, arcsin_power(0, 20));
-  CPPUNIT_ASSERT_EQUAL(M_PI / 2, arcsin_power(1, 70));
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(0.0, arcsin_power(0, 15), 0.01);
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(M_PI / 2, arcsin_power(1, 15), 0.15);
+  CPPUNIT_ASSERT_DOUBLES_EQUAL(M_PI / 2, arcsin_power(1, 10000), 0.01);
 }
