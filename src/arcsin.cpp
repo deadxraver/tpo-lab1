0846cd1e678d63e0f1a1735a3548f0bffa063c36
chimi <deadxraver@yandex.ru>
2026-03-06 21:41:59 +0300

    arcsin logic done

--- src/arcsin.cpp
+++ src/arcsin.cpp
@@ -1,7 +1,28 @@
 #include "arcsin.hpp"
 
+#include <cmath>
+#include <exception>
+#include <string>
+
+unsigned long factorial(unsigned int n) {
+  unsigned long res = 1;
+  for (unsigned int i = 1; i <= n; ++i) {
+    res *= i;
+  }
+  return res;
+}
+
 double arcsin_power(double x, unsigned int n) {
-  double res = x;
-  // TODO: 
+  if (x > 1 || x < -1) {
+    throw std::string("|x| > 1");
+  }
+  if (n > 70) {
+    throw std::string("n > 70, double won't handle this...");
+  }
+  double res = 0;
+  for (unsigned int i = 0; i < n; ++i) {
+    res += factorial(2 * i) / (std::pow(2, 2 * i) * std::pow(factorial(i), 2))
+      * std::pow(x, 2 * i + 1) / (2 * i + 1);
+  }
   return res;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,18 @@
 #include "arcsin.hpp"
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     std::cerr << "Wrong number of arguements, usage: " << argv[0] << " <x> <n>\n";
     return -EINVAL;
   }
-  std::cout << arcsin_power(std::stoi(argv[1]), std::stoi(argv[2])) << std::endl;
+  try {
+    std::cout << arcsin_power(std::stod(argv[1]), std::stoul(argv[2])) << std::endl;
+  } catch (std::string s) {
+    std::cerr << "Error: " << s << std::endl;
+    return -1;
+  }
   return 0;
 }
