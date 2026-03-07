63600cf730e501c89a6958061b152f3541f9bc7e
chimi <deadxraver@yandex.ru>
2026-03-07 14:44:25 +0300

    moved to gtest

--- 1-arcsin/test/arcsin_test.cpp
+++ 1-arcsin/test/arcsin_test.cpp
@@ -1,15 +1,17 @@
-#include "arcsin_test.hpp"
+#include <gtest/gtest.h>
+
+#include "arcsin.hpp"
 
 #include <cmath>
 #include <iostream>
 
-void ArcsinTestCase::overflow_test() {
-  CPPUNIT_ASSERT_THROW(arcsin_power(1.1, 10), std::string);
-  CPPUNIT_ASSERT_NO_THROW(arcsin_power(0.5, 15));
+TEST(ArcsinTest, ValidationTest) {
+  EXPECT_THROW(arcsin_power(1.1, 10), std::string);
+  EXPECT_NO_THROW(arcsin_power(0.5, 15));
 }
 
-void ArcsinTestCase::result_test() {
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(0.0, arcsin_power(0, 15), 0.01);
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(M_PI / 2, arcsin_power(1, 15), 0.15);
-  CPPUNIT_ASSERT_DOUBLES_EQUAL(M_PI / 2, arcsin_power(1, 10000), 0.01);
+TEST(ArcsinTest, ResultTest) {
+  EXPECT_NEAR(0.0, arcsin_power(0, 15), 0.01);
+  EXPECT_NEAR(M_PI / 2, arcsin_power(1, 15), 0.15);
+  EXPECT_NEAR(M_PI / 2, arcsin_power(1, 10000), 0.01);
 }
