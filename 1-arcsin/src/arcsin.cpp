b62eb814b81cc15a76bcfa3f4352ca653c72a4ac
chimi <deadxraver@yandex.ru>
2026-03-07 21:43:30 +0300

    remove factorial function as it is not used anymore

--- 1-arcsin/src/arcsin.cpp
+++ 1-arcsin/src/arcsin.cpp
@@ -4,14 +4,6 @@
 #include <exception>
 #include <string>
 
-double factorial(unsigned int n) {
-  double res = 1;
-  for (unsigned int i = 1; i <= n; ++i) {
-    res *= i;
-  }
-  return res;
-}
-
 double arcsin_power(double x, unsigned int n) {
   if (x > 1 || x < -1) {
     throw std::string("|x| > 1");
