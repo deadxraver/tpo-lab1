4163c10865bd7d1b639c1a6609fc0e0cf742ca16
chimi <deadxraver@yandex.ru>
2026-03-07 21:41:50 +0300

    add tests to check correctness of sort

--- 2-bubble-sort/test/bubblesort_test.cpp
+++ 2-bubble-sort/test/bubblesort_test.cpp
@@ -10,4 +10,14 @@ TEST(BubbleSortTest, PropertyTest) {
   std::size_t expected_sz = vec.size();
   bubble_sort(vec);
   EXPECT_EQ(expected_sz, vec.size());
+  for (size_t i = 1; i < vec.size(); ++i) {
+    EXPECT_TRUE(vec[i - 1] <= vec[i]);
+  }
+}
+
+TEST(BubbleSortTest, SortCorrectnessTest) {
+  std::vector<double> vec = {208, 0, -0.999999, 7, 1.01, -0.000001};
+  std::vector<double> sorted = {-0.999999, -0.000001, 0, 1.01, 7, 208};
+  bubble_sort(vec);
+  EXPECT_EQ(vec, sorted);
 }
