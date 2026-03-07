afea82a92892b181b1f0d037376b6b3744653c04
chimi <deadxraver@yandex.ru>
2026-03-07 18:51:28 +0300

    another property to test

--- 2-bubble-sort/test/bubblesort_test.cpp
+++ 2-bubble-sort/test/bubblesort_test.cpp
@@ -6,4 +6,8 @@ TEST(BubbleSortTest, PropertyTest) {
   std::vector<double> vec;
   bubble_sort(vec);
   EXPECT_EQ(vec.size(), 0);
+  vec = {0, 0, 1, 0, -1};
+  std::size_t expected_sz = vec.size();
+  bubble_sort(vec);
+  EXPECT_EQ(expected_sz, vec.size());
 }
