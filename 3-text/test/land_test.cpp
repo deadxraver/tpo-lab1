8b07e19bcda9d2ee9c946c05eec8bc00d7d3a4ad
chimi <deadxraver@yandex.ru>
2026-03-09 23:12:25 +0300

    add underground tests

--- 3-text/test/land_test.cpp
+++ 3-text/test/land_test.cpp
@@ -11,3 +11,20 @@ TEST(LandTest, EarthTest) {
   e.open();
   EXPECT_NO_THROW(e.underground());
 }
+
+TEST(LandTest, UndergroundTest) {
+  Underground ug;
+  EXPECT_FALSE(ug.is_open());
+  EXPECT_TRUE(ug.is_dusty());
+  EXPECT_TRUE(ug.is_dark());
+  EXPECT_EQ(0, ug.visibility());
+  EXPECT_THROW(ug.clean_up(Person("", 1)), std::string);
+  EXPECT_NO_THROW(ug.clean_up(Person("", 51)));
+  EXPECT_TRUE(ug.is_open());
+  EXPECT_EQ(25, ug.visibility());
+  EXPECT_THROW(ug.light_up(Person("", 0, false)), std::string);
+  EXPECT_TRUE(ug.is_dark());
+  EXPECT_NO_THROW(ug.light_up(Person("", 0, true)));
+  EXPECT_FALSE(ug.is_dark());
+  EXPECT_EQ(45, ug.visibility());
+}
